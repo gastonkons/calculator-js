5e5e32d8aa6656a2c95f151595af29185dcfd1ba
gastonkons <gastonkons@gmail.com>
2020-02-02 18:12:55 -0300

    Agregando funcionalidad, keyEvents y descripcion de lo agregado

--- js/scripts.js
+++ js/scripts.js
@@ -49,6 +49,7 @@ function addNumberInput(value) {
     inputScreen.value = inputValue + value;
 }
 
+// Setea la operación
 function setOperation(operator) {
     const inputScreenValue = document.querySelector('.calculator__screen').value;
     this.operator = operator;
@@ -68,20 +69,23 @@ function calculation(){
     if ( this.operator === '+' && inputScreen.value !== "" ){
         total = firstValue + secondValue;
     } else
+    // Operacion Resta
     if (this.operator === '-' && inputScreen.value !== "" ){
         if (firstValue !== 0) {
             total = firstValue - secondValue;
         } else {
             total = secondValue;
         }
-    } else 
+    } else
+    // Operacion Multiplicación
     if (this.operator === '*' && inputScreen.value !== "") {
         if (firstValue !== 0) {
             total = firstValue * secondValue;
         } else {
             total = secondValue;
         }
-    } else 
+    } else
+    // Operación División
     if (this.operator === '/' && inputScreen.value !== "") {
         if (firstValue !== 0) {
             total = firstValue / secondValue;
@@ -89,14 +93,20 @@ function calculation(){
             total = secondValue;
         }
     } else
+    // Operación Porcentaje
     if (this.operator === "%" && inputScreen.value !== "" ){
         total = secondValue / 100;
     } else 
+    // Operacion Cambiar de Signo
     if (this.operator === "+/-" && inputScreen.value !== "" ){
         if (secondValue > 0) {
             total = -secondValue;
         } 
-    } else { total = firstValue;}
+    } 
+    // En caso de ser un string vacío mantiene el valor
+    else {
+        total = firstValue;
+    }
 
     total = transformPointToComma(total);
     this.inputValueMemo = total;
@@ -104,6 +114,7 @@ function calculation(){
     inputScreen.placeholder = total;
 }
 
+// Transforma la comma de tipo string a un punto de tipo number
 function transformCommaToPoint (value) {
     if ( typeof value !== "number" ) {
         let resultTransform = value.replace(',', '.');
@@ -112,19 +123,94 @@ function transformCommaToPoint (value) {
     return value;
 }
 
+// Transforma el punto de tipo number a una comma de tipo string
 function transformPointToComma(value) {
     let resultTransform = value.toString();
     resultTransform = resultTransform.replace('.', ',');
     return resultTransform;
 }
 
-
-
-// ########################################################
-
+// Limpiar consola 
 function clearScreen(value) {
     const inputScreen = document.querySelector(".calculator__screen");
     inputScreen.value = value;
     inputValueMemo = 0;
     this.operator = null;
 }
+
+// ########################################################
+// Keywords
+
+function keyDowns(){
+    document.addEventListener('keydown', keyPress, "false");
+}
+
+function keyPress(e){
+    const key = transformPointToComma(e.key);
+    console.log(key);
+    key === '1' ? addNumberInput(1) : null;
+    key === '2' ? addNumberInput(2) : null;
+    key === '3' ? addNumberInput(3) : null;
+    key === '4' ? addNumberInput(4) : null;
+    key === '5' ? addNumberInput(5) : null;
+    key === '6' ? addNumberInput(6) : null;
+    key === '7' ? addNumberInput(7) : null;
+    key === '8' ? addNumberInput(8) : null;
+    key === '9' ? addNumberInput(9) : null;
+    key === '0' ? addNumberInput(0) : null;
+    key === ',' ? addNumberInput(",") : null;
+    
+    key === '+' ? setOperation('+') : null;
+    key === '-' ? setOperation('-') : null;
+    key === '*' ? setOperation('*') : null;
+    key === '/' ? setOperation('/') : null;
+    key === '%' ? setOperation('%') : null;
+    key === 'F9' ? setOperation('+/-') : null;
+
+    key === 'Enter' ? calculation() : null;
+    key === 'Backspace' ? lastChar() : null;
+
+    key === 'Escape' ? clearScreen('0') : null;
+    
+
+    /*
+        value === "0" ? addNumberInput(0) : null;
+    value === "1" ? addNumberInput(1) : null;
+    value === "2" ? addNumberInput(2) : null;
+    value === "3" ? addNumberInput(3) : null;
+    value === "4" ? addNumberInput(4) : null;
+    value === "5" ? addNumberInput(5) : null;
+    value === "6" ? addNumberInput(6) : null;
+    value === "7" ? addNumberInput(7) : null;
+    value === "8" ? addNumberInput(8) : null;
+    value === "9" ? addNumberInput(9) : null;
+    value === "," ? addNumberInput(',') : null;
+
+    value === "+" ? setOperation('+') : null;
+    value === "-" ? setOperation('-') : null;
+    value === "x" ? setOperation('*') : null;
+    value === "/" ? setOperation('/') : null;
+    value === "%" ? setOperation('%') : null;
+    value === "+/-" ? setOperation('+/-') : null;
+
+    value === "=" ? calculation() : null;
+
+    value === "AC" ? clearScreen('0') : null;
+    */
+}
+
+function lastChar(){
+    const inputScreen = document.querySelector('.calculator__screen');
+    let value = inputScreen.value;
+    console.log(typeof inputScreen.value);
+    value = value.replace(value[value.length - 1], "");
+    console.log(value);
+    if (value === "" || value === "0"){
+        inputScreen = "0";
+        return;
+    }
+    inputScreen.value = value;
+    
+}
+
+window.addEventListener("load", keyDowns, "false");
